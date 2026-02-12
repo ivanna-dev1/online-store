c0a2678fb6846031299ca7f5ab842c9a0c2c20a3
Ivanna <ivannakucher0102@gmail.com>
2026-02-12 17:43:23 +0200

    feat: Implement initial product display with search and category filtering functionality using new product data and card components.

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,54 @@ const initialList = products;
 function App() {
   const [products, setProducts] = useState(initialList);
   const [searchTerm, setSearchTerm] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("All");
 
-  const filteredProducts=products.filter((item)=>item.name.toLowerCase().includes(searchTerm.toLowerCase()));
-  
+  const filteredProducts = products.filter((item) => {
+    const matchesSearch = item.name
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase());
+    const matchesCategory =
+      selectedCategory === item.category || selectedCategory === "All";
+    return matchesSearch && matchesCategory;
+  });
+  const categories = [
+    "All",
+    ...new Set(products.map((product) => product.category)),
+  ];
   return (
     <div className="p-4">
       <h1 className="text-3xl font-bold text-red-500 underline">
         Shop Project
       </h1>
-      <input className="m-3 p-2 border border-gray-300 rounded-md w-full md:w-1/2 lg:w-1/3" type="text" placeholder="Search..." value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+      <input
+        className="m-3 p-2 border border-gray-300 rounded-md w-full md:w-1/2 lg:w-1/3"
+        type="text"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <section>
+        {categories.map((category) => (
+          <button
+            key={category}
+            className={
+              selectedCategory === category
+                ? "bg-blue-500 text-white px-4 py-2 rounded-lg"
+                : "bg-gray-500 text-white px-4 py-2 rounded-lg"
+            }
+            onClick={() => setSelectedCategory(category)}
+          >
+            {category}
+          </button>
+        ))}
+      </section>
       <ul className="m-5 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
         {filteredProducts.map((item) => (
           <ProductCard key={item.id} product={item} />
         ))}
       </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/products.js
+++ src/products.js
@@ -1,7 +1,7 @@
 const products = [
     {
         id: 1,
-        name: "Product 1",
+        name: "Apple 1",
         category: "Category 1",
         price: 10,
         description: "Description of Product 1",
@@ -9,15 +9,15 @@ const products = [
     },
     {
         id: 2,
-        name: "Product 2",
+        name: "Banana 2",
         category: "Category 2",
         price: 20,
         description: "Description of Product 2",
-        image: "https://delikates.ua/images/product/yabluko-chervone.jpg"
+        image: "https://static2.tavriav.ua/media/images/8/cd/d0/53/8cdd0533ed2b_10528_main.webp"
     },
     {
         id: 3,
-        name: "Product 3",
+        name: "Apple 3",
         category: "Category 3",
         price: 30,
         description: "Description of Product 3",
@@ -25,27 +25,27 @@ const products = [
     },
     {
         id: 4,
-        name: "Product 4",
-        category: "Category 4",
+        name: "Banana 4",
+        category: "Category 2",
         price: 40,
         description: "Description of Product 4",
-        image: "https://delikates.ua/images/product/yabluko-chervone.jpg"
+        image: "https://static2.tavriav.ua/media/images/8/cd/d0/53/8cdd0533ed2b_10528_main.webp"
     },
     {
         id: 5,
-        name: "Product 5",
-        category: "Category 5",
+        name: "Apple 5",
+        category: "Category 3",
         price: 50,
         description: "Description of Product 5",
         image: "https://delikates.ua/images/product/yabluko-chervone.jpg"
     },
     {
         id: 6,
-        name: "Product 6",
-        category: "Category 6",
+        name: "Banana 6",
+        category: "Category 2",
         price: 60,
         description: "Description of Product 6",
-        image: "https://delikates.ua/images/product/yabluko-chervone.jpg"
+        image: "https://static2.tavriav.ua/media/images/8/cd/d0/53/8cdd0533ed2b_10528_main.webp"
     }
 ]
 
