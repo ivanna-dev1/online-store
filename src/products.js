7054e341d018dab2e39d289c9412f7c8c67160bd
Ivanna <ivannakucher0102@gmail.com>
2026-02-23 15:56:39 +0200

    feat: add initial product data to products.js

--- src/products.js
+++ src/products.js
@@ -1,52 +1,106 @@
 const products = [
-    {
-        id: 1,
-        name: "Apple 1",
-        category: "Category 1",
-        price: 10,
-        description: "Description of Product 1",
-        image: "https://delikates.ua/images/product/yabluko-chervone.jpg"
-    },
-    {
-        id: 2,
-        name: "Banana 2",
-        category: "Category 2",
-        price: 20,
-        description: "Description of Product 2",
-        image: "https://static2.tavriav.ua/media/images/8/cd/d0/53/8cdd0533ed2b_10528_main.webp"
-    },
-    {
-        id: 3,
-        name: "Apple 3",
-        category: "Category 3",
-        price: 30,
-        description: "Description of Product 3",
-        image: "https://delikates.ua/images/product/yabluko-chervone.jpg"
-    },
-    {
-        id: 4,
-        name: "Banana 4",
-        category: "Category 2",
-        price: 40,
-        description: "Description of Product 4",
-        image: "https://static2.tavriav.ua/media/images/8/cd/d0/53/8cdd0533ed2b_10528_main.webp"
-    },
-    {
-        id: 5,
-        name: "Apple 5",
-        category: "Category 3",
-        price: 50,
-        description: "Description of Product 5",
-        image: "https://delikates.ua/images/product/yabluko-chervone.jpg"
-    },
-    {
-        id: 6,
-        name: "Banana 6",
-        category: "Category 2",
-        price: 60,
-        description: "Description of Product 6",
-        image: "https://static2.tavriav.ua/media/images/8/cd/d0/53/8cdd0533ed2b_10528_main.webp"
-    }
-]
+  {
+    id: 1,
+    name: "Apple 1",
+    category: "Category 1",
+    price: 10,
+    description: "Description of Product 1",
+    image: "https://delikates.ua/images/product/yabluko-chervone.jpg",
+  },
+  {
+    id: 2,
+    name: "Banana 2",
+    category: "Category 2",
+    price: 20,
+    description: "Description of Product 2",
+    image:
+      "https://static2.tavriav.ua/media/images/8/cd/d0/53/8cdd0533ed2b_10528_main.webp",
+  },
+  {
+    id: 3,
+    name: "Avocado 3",
+    category: "Category 3",
+    price: 30,
+    description: "Description of Product 3",
+    image: "https://soncesad.com/assets/images/products/1945/.jpeg",
+  },
+  {
+    id: 4,
+    name: "Tomato 4",
+    category: "Category 2",
+    price: 40,
+    description: "Description of Product 4",
+    image: "https://agrolife.ua/media/blog/post/pomidor_1_1__1.jpeg",
+  },
+  {
+    id: 5,
+    name: "Orange 5",
+    category: "Category 3",
+    price: 50,
+    description: "Description of Product 5",
+    image:
+      "https://fruit-time.ua/images/cache/products/b4/apelsin-ispaniya-imp-500x500.jpeg",
+  },
+  {
+    id: 6,
+    name: "Lemon 6",
+    category: "Category 2",
+    price: 60,
+    description: "Description of Product 6",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg3mho6cMKE0n1zgPFjwVA_sC_pwckcHvIeQ&s",
+  },
+  {
+    id: 7,
+    name: "Grapes 7",
+    category: "Category 1",
+    price: 10,
+    description: "Description of Product 7",
+    image: "https://soncesad.com/assets/images/products/1945/.jpeg",
+  },
+  {
+    id: 8,
+    name: "Pear 8",
+    category: "Category 2",
+    price: 20,
+    description: "Description of Product 8",
+    image:
+      "https://static2.tavriav.ua/media/images/8/cd/d0/53/8cdd0533ed2b_10528_main.webp",
+  },
+  {
+    id: 9,
+    name: "Apple 9",
+    category: "Category 3",
+    price: 30,
+    description: "Description of Product 9",
+    image: "https://soncesad.com/assets/images/products/1945/.jpeg",
+  },
+  {
+    id: 10,
+    name: "Orange 10",
+    category: "Category 2",
+    price: 40,
+    description: "Description of Product 10",
+    image:
+      "https://static2.tavriav.ua/media/images/8/cd/d0/53/8cdd0533ed2b_10528_main.webp",
+  },
+  {
+    id: 11,
+    name: "Tomato 11",
+    category: "Category 3",
+    price: 50,
+    description: "Description of Product 11",
+    image: "https://agrolife.ua/media/blog/post/pomidor_1_1__1.jpeg",
+  },
+  {
+    id: 12,
+    name: "Banana 12",
+    category: "Category 2",
+    price: 60,
+    description: "Description of Product 12",
+    image:
+      "https://static2.tavriav.ua/media/images/8/cd/d0/53/8cdd0533ed2b_10528_main.webp",
+  },
+];
 
-export default products
+export default products;
